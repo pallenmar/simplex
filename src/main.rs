49aa381773418f2ae9aa35f370af61a461fefd40
Philip Mar <pallenmar@gmail.com>
2026-04-23 19:37:03 -0400

    fix: add row_times_col

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,29 @@ fn main() {
 	println!("{}", a_dot_b);
 }
 
+fn row_times_col(a: &Vec<f32>, b: &Vec<Vec<f32>>) -> f32{
+	let mut product = 0.;
+	for i in 0..a.len(){
+		product += a[i] * b[i][0];
+	}
+	product
+}
+
 fn transpose_to_row(a: &Vec<Vec<f32>>) -> Vec<f32>{
-
 	let mut vec: Vec<f32> = vec![0.0; a.len()];
-
 	for i in 0..a.len(){
 		vec[i] = a[i][0];
 	}
-
-	return vec;
-
+	vec
 }
 
 fn transpose_to_col(a: &Vec<f32>) -> Vec<Vec<f32>>{
 	let mut vec: Vec<Vec<f32>> = vec![vec![0.0]; a.len()];
-	
 	for i in 0..a.len(){
 		vec[i][0] = a[i];
 	}
 
-	return vec;
+	vec
 }
 
 // changed to vec, since i wanted it to be easier to transpose things
@@ -67,6 +70,14 @@ mod tests {
 
 		assert_eq!(ans, transpose_to_col(&a));
 		assert_eq!(transpose_to_row(&transpose_to_col(&a)), a);
+	}
+
+	#[test]
+	fn test_row_times_col() {
+		let a = vec![1.0, 2.0];
+		let b = vec![vec![1.0], vec![2.0]];
+		
+		assert_eq!(5., row_times_col(&a, &b));
 	}
 
 }
